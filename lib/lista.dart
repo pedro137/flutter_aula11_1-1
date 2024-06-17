c2ce928ca9268de955e1eddb31b791d9ba0c5e2b
Pedro Falcheti <pedrohenriquefalcheti@gmail.com>
2024-06-17 20:56:23 -0300

    .

--- lib/lista.dart
+++ lib/lista.dart
@@ -1,20 +1,33 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class Lista extends StatelessWidget {
-  List<String> dias = [ 'domingo', 'segunda', 'terça'];
+  List<String> dias = [
+    'domingo',
+    'segunda',
+    'terça',
+    'quarta',
+    'quinta',
+    'sexta',
+    'sábado'
+  ];
 
-  Lista({super.key});
+  Lista({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // return const Placeholder();
     return ListView.builder(
+      scrollDirection: Axis.horizontal, // Define o scroll horizontal
       itemCount: dias.length,
       itemBuilder: (context, index) {
+        // Verifica se é o último item da lista para não adicionar SizedBox depois
+        bool isLastItem = index == dias.length - 1;
+
         return Row(
-          	children: [
-	            Text(dias[index]),
-            ],
+          children: [
+            Text(dias[index]),
+            // Adiciona SizedBox de largura 30 entre os itens, exceto após o último item
+            if (!isLastItem) SizedBox(width: 30),
+          ],
         );
       },
     );
